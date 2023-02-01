ca1428900e8a99e9b20df7d74487726b5c8e0066
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2023-02-01 20:12:57 +0000

    Fix compiler errors

--- src/contracts/project.ts
+++ src/contracts/project.ts
@@ -1,3 +1,5 @@
+import { URL } from 'url';
+
 export default interface Project {
     id: string;
     name: string;
@@ -5,6 +7,6 @@ export default interface Project {
     updated_at: string;
     description: string;
     website?: string;
-    html_url?: string;
+    html_url: URL;
     language: string;
 };
